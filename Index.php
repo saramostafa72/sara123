fefbf3ae9cada093ad56d3fd1c0aeb54fe2cd073
saramostafa72 <99365927+saramostafa72@users.noreply.github.com>
2022-03-01 17:19:50 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+        <head>
+        <meta charset="UTF-8">
+        </head>
+    <body>
+        <h1>MSP-TASK</h1>
+     <form action="profile.php" method="get">
+          <label> <b>username : </b></label>
+                <input type="text" name="username">
+                <br>
+                <br>
+          <label> <b>password : </b></label>
+                <input type="password" name="password">
+                <br>
+                <br>
+          <button type="submit">submit</button>
+      </form>
+    </body>
+</html>
---
+++ Profile.php
@@ -0,0 +1,14 @@
+<?php
+ echo "your username is " . $_GET['username'] ."<br>"."<br>";
+ echo "your password is " . $_GET['password'] ."<br>"."<br>";
+?>
+<br> 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <button onclick="window.alert('TASK SUCCEESS')">click here</button>
+    </body>
+</html>
